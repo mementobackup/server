b32385119e5c3ddb156df4c4411a7d03d21b72ac
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-08-24 22:57:48 +0200

    Reorganized code

--- src/server/server.go
+++ src/server/server.go
@@ -6,6 +6,15 @@
 */
 
 package server
+import (
+	"github.com/op/go-logging"
+	"bufio"
+	"net"
+	"bitbucket.org/ebianchi/memento-common/common"
+	"github.com/go-ini/ini"
+	"server/network"
+	"encoding/json"
+)
 
 var SECT_RESERVED = []string{"DEFAULT", "general", "database", "dataset"}
 
@@ -16,4 +25,50 @@ func contains(s []string, e string) bool {
 		}
 	}
 	return false
+}
+
+func exec_command(log *logging.Logger, section *ini.Section, command string) {
+	var buff *bufio.Reader
+	var conn net.Conn
+	var cmd common.JSONMessage
+	var res common.JSONResult
+	var err error
+	var result []byte
+
+	if section.Key(command).String() != "" {
+		conn, err = network.Getsocket(section)
+		if err != nil {
+			log.Error("Error when executing " + command + ": " + err.Error())
+			return
+		}
+		defer conn.Close()
+
+		buff = bufio.NewReader(conn)
+
+		cmd = common.JSONMessage{}
+		cmd.Context = "system"
+		cmd.Command.Name = "exec"
+		cmd.Command.Value = section.Key(command).String()
+
+		if err = cmd.Send(conn); err != nil {
+			log.Error("Sending " + command + " failed: " + err.Error())
+		} else {
+			result, err = buff.ReadBytes('\n')
+			if err != nil {
+				log.Error("Receive " + command + " result failed: " + err.Error())
+			}
+
+			err = json.Unmarshal(result, &res)
+			if err != nil {
+				log.Error("Responde result for " + command + " failed: " + err.Error())
+			}
+
+			if res.Result == "ok" {
+				// TODO: manage corrected execution for remote command
+			} else {
+				log.Error("Error in " + command + " execution: " + res.Message)
+			}
+		}
+		log.Debug("Executed " + command)
+	}
 }
