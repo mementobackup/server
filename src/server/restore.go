8cafd7579849ee66c76fa16f553236ea457a45b1
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-08-25 00:50:03 +0200

    Extended restore function

--- src/server/restore.go
+++ src/server/restore.go
@@ -6,11 +6,24 @@
 */
 
 package server
+
 import (
-	"github.com/op/go-logging"
 	"github.com/go-ini/ini"
+	"github.com/op/go-logging"
 )
 
 func Restore(log *logging.Logger, cfg *ini.File, grace string) {
+	dataset := cfg.Section("general").Key("dataset").MustInt()
+
+	for _, section := range cfg.Sections() {
+		if !contains(SECT_RESERVED, section.Name()) {
+			if section.Key("type").String() == "file" {
+				restorefile(log, cfg, grace, dataset)
+			}
+		}
+	}
+}
+
+func restorefile(log *logging.Logger, cfg *ini.File, grace, dataset string) {
 
 }
